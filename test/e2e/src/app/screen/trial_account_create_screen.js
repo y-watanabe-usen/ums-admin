1c0c2b4f19f7785be8205599b27b5e8edf3b8081
Mayuu Onishi <m-onishi@usen-next.jp>
2020-08-13 12:31:04 +0900

    お試しアカウント検索/発行画面のテストの追加と修正

---
+++ test/e2e/src/app/screen/trial_account_create_screen.js
@@ -0,0 +1,24 @@
+const { Builder, By, Key, Capabilities, until } = require('selenium-webdriver');
+
+const AuthedScreen = require(`${__dirname}/common/authed_screen`);
+
+module.exports = class TrialAccountDetailScreen extends AuthedScreen {
+
+    get count() {
+        return this.driver.findElement(By.xpath('//*[@id="count"]')).getText();
+    }
+
+    async clickTrialMenuTrialAccountCreate() {
+        await this.driver.findElement(By.xpath('/html/body/div[2]/div/div[1]/div/div/ul/li[2]/a')).click();
+    }
+
+    async clickBtnTrialAccountCreate() {
+        await this.driver.findElement(By.id('bt_download')).click();
+    }
+
+    async inputCount(count) {
+        await this.driver.findElement(By.name('count')).sendKeys(count);
+    }
+
+
+}
