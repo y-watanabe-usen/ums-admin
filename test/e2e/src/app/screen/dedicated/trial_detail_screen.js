4555d3e52465d69027315e3ba154fc04a8bdf058
Mayuu Onishi <m-onishi@usen-next.jp>
2020-09-15 14:59:08 +0900

    お試しアカウント詳細画面のテストの追加

--- test/e2e/src/app/screen/dedicated/trial_detail_screen.js
+++ test/e2e/src/app/screen/dedicated/trial_detail_screen.js
@@ -33,7 +33,136 @@ module.exports = class TrialDetailScreen extends AuthedScreen {
         return this.driver.findElement(By.xpath('//*[@id="grid"]/tbody/tr/td[7]')).getText();
     }
 
+    get viewingHistoryTotal() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[1]/td')).getText();
+    }
+
+    get viewingHistory1st() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[2]/td')).getText();
+    }
+
+    get viewingHistory2nd() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[3]/td')).getText();
+    }
+
+    get viewingHistory3rd() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[4]/td')).getText();
+    }
+
+    get viewingHistory4th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[5]/td')).getText();
+    }
+
+    get viewingHistory5th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[6]/td')).getText();
+    }
+
+    get viewingHistory6th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[7]/td')).getText();
+    }
+
+    get viewingHistory7th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[8]/td')).getText();
+    }
+
+    get viewingHistory8th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[9]/td')).getText();
+    }
+
+    get viewingHistory9th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[10]/td')).getText();
+    }
+
+    get viewingHistory10th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[11]/td')).getText();
+    }
+
+    get viewingHistory11th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[12]/td')).getText();
+    }
+
+    get viewingHistory12th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[13]/td')).getText();
+    }
+
+    get viewingHistory13th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[14]/td')).getText();
+    }
+
+    get viewingHistory14th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[15]/td')).getText();
+    }
+
+    get viewingHistory15th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[16]/td')).getText();
+    }
+
+    get viewingHistory16th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[17]/td')).getText();
+    }
+
+    get viewingHistory17th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[18]/td')).getText();
+    }
+
+    get viewingHistory18th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[19]/td')).getText();
+    }
+
+    get viewingHistory19th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[20]/td')).getText();
+    }
+
+    get viewingHistory20th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[21]/td')).getText();
+    }
+
+    get viewingHistory21st() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[22]/td')).getText();
+    }
+
+    get viewingHistory22nd() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[23]/td')).getText();
+    }
+
+    get viewingHistory23rd() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[24]/td')).getText();
+    }
+
+    get viewingHistory24th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[25]/td')).getText();
+    }
+
+    get viewingHistory25th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[26]/td')).getText();
+    }
+
+    get viewingHistory26th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[27]/td')).getText();
+    }
+
+    get viewingHistory27th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[28]/td')).getText();
+    }
+
+    get viewingHistory28th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[29]/td')).getText();
+    }
+
+    get viewingHistory29th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[30]/td')).getText();
+    }
+
+    get viewingHistory30th() {
+        return this.driver.findElement(By.xpath('//*[@id="grid3"]/tbody/tr[31]/td')).getText();
+    }
+
     async clickBtnReturnSearchList() {
         await this.driver.findElement(By.id('bt_back')).click();
     }
+
+    async clickBtnDownloadViewingHistory() {
+        await this.driver.findElement(By.id('bt_download')).click();
+    }
+
 }
--- test/e2e/src/app/tests/dedicated/trial_detail_tests.js
+++ test/e2e/src/app/tests/dedicated/trial_detail_tests.js
@@ -62,6 +62,37 @@ exports.testMain = () => {
       assert.deepEqual(await trialDetailScreen.issueDate, '2020-08-05');
       assert.deepEqual(await trialDetailScreen.firstAuthenticationDatetimes, '2020-09-14 11:25:44');
       assert.deepEqual(await trialDetailScreen.expireDate, '2020-09-27');
+      assert.deepEqual(await trialDetailScreen.viewingHistoryTotal, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory1st, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory2nd, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory3rd, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory4th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory5th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory6th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory7th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory8th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory9th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory10th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory11th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory12th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory13th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory14th, '10時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory15th, '11時間11分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory16th, '12時間22分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory17th, '13時間33分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory18th, '0時間01分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory19th, '0時間02分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory20th, '22時間44分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory21st, '2時間30分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory22nd, '1時間02分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory23rd, '2時間33分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory24th, '11時間11分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory25th, '9時間59分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory26th, '23時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory27th, '23時間59分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory28th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory29th, '0時間00分');
+      assert.deepEqual(await trialDetailScreen.viewingHistory30th, '0時間00分');
       // ****************************
       // ** 後始末
       // ****************************
@@ -88,6 +119,40 @@ exports.testMain = () => {
       // ** 検証
       // ****************************
       assert.deepEqual(await driver.getCurrentUrl(), Const.ADMIN_URL + 'dedicated/trial_search');
+      // ****************************
+      // ** 後始末
+      // ****************************
+    });
+    it('視聴履歴がダウンロードできること', async () => {
+      // ****************************
+      // ** 準備
+      // ****************************
+      const loginScreen = new LoginScreen(driver);
+      const trialSearchScreen = new TrialSearchScreen(driver);
+      const trialDetailScreen = new TrialDetailScreen(driver);
+      await loginScreen.access();
+      await loginScreen.inputCode('admin');
+      await loginScreen.inputPassword('!QAZ2wsx');
+      await loginScreen.clickBtnLogin();
+      await trialSearchScreen.clickBtnTrial();
+      await trialSearchScreen.clickBtnTrialAccountSearch();
+      await trialSearchScreen.clickBtnDetail();
+      // ****************************
+      // ** 実行
+      // ****************************
+      await trialDetailScreen.clickBtnDownloadViewingHistory();
+      sleep.sleep(1);
+      // ****************************
+      // ** 検証
+      // ****************************
+      // ファイル名取得
+      const stdout = execSync(`ls ${Const.DOWNLOAD_PATH}`);
+      const csvFilename = stdout.toString().replace("\n", "");
+      // ファイル読み込み
+      const actual = fs.readFileSync(`${Const.DOWNLOAD_PATH}/${csvFilename}`).toString();
+      const expected = fs.readFileSync(`${Dir.filesDedicated}/expected_viewing_history.csv`).toString();
+      // ファイル内容の比較
+      await assert.deepEqual(actual, expected);
       // ****************************
       // ** 後始末
       // ****************************
