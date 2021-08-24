51a832e06ddc4984ce740510cef6404b0e3a978e
m-gima <masumi.gima@gmail.com>
2021-08-24 12:24:25 +0900

    test: .jsテスト削除

--- test/e2e/src/app/screen/common/not_authed_screen.js
+++
@@ -1,6 +0,0 @@
-const { Builder, By, Key, Capabilitiesi, until } = require('selenium-webdriver');
-const { Dir, Const, Utils } = require('lib');
-
-const Screen = require(`${Dir.screenCommon}/screen`);
-
-module.exports = class NotAuthedScreen extends Screen { }
--- test/e2e/src/app/screen/common/screen.js
+++
@@ -1,5 +0,0 @@
-module.exports = class Screen {
-  constructor(driver) {
-    this.driver = driver;
-  }
-}
